d438d33e7dee987140ea656ad977753b4992a57f
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2026-01-02 21:04:45 -0800

    docs: add license headers
    Add standard MPL-2.0 license to the top of every Rust source code file.

--- lib/wf_inv_auth_scanning/src/process.rs
+++ lib/wf_inv_auth_scanning/src/process.rs
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: MPL-2.0
+//
+// Copyright © 2025 RemasteredArch
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0. If a
+// copy of the Mozilla Public License was not distributed with this file, You can obtain one at
+// <https://mozilla.org/MPL/2.0/>.
+
 use std::ffi;
 
 use windows::Win32::{

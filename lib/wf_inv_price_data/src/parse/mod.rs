9693632902588db21e7b7f8007d64cf27aeb4c5f
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2026-08-15 17:54:52 -0700

    chore: add/update license headers
    - Added standard MPL-2.0 license notice to the top of every Rust source
      code file that was previously missing one.
    - Updated existing license notices in files that didn't include 2026 in
      the copyright years despite having changes made to them in 2026.

--- lib/wf_inv_price_data/src/parse/mod.rs
+++ lib/wf_inv_price_data/src/parse/mod.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 //
-// Copyright © 2025 RemasteredArch
+// Copyright © 2025-2026 RemasteredArch
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0. If a
 // copy of the Mozilla Public License was not distributed with this file, You can obtain one at
--- src/table/mod.rs
+++ src/table/mod.rs
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: MPL-2.0
+//
+// Copyright © 2026 RemasteredArch
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0. If a
+// copy of the Mozilla Public License was not distributed with this file, You can obtain one at
+// <https://mozilla.org/MPL/2.0/>.
+
 use std::fmt::Display;
 
 pub use column::{Column, ColumnType, ErasedColumn};
